9a0785e5f857865741e2a8dc07c6a6b36db320f3
dlguszoo <lhjivy1024@gmail.com>
2025-11-02 18:03:46 +0900

    [#50] feat: Thumbnail 이미지만 표시하는 KFImage 컴포넌트 구현

---
+++ HonestHouse/HonestHouse/Sources/Presentation/Archive/Component/CachedThumbnailImage.swift
@@ -0,0 +1,27 @@
+//
+//  CachedThumbnailImage.swift
+//  HonestHouse
+//
+//  Created by 이현주 on 11/1/25.
+//
+
+import SwiftUI
+import Kingfisher
+
+/// Thumbnail 이미지만 표시 (그리드, 그룹 대표 이미지 등)
+struct CachedThumbnailImage: View {
+    let url: String
+
+    var body: some View {
+        KFImage(URL(string: url))
+            .placeholder {
+                Color.gray.opacity(0.3)
+            }
+            .retry(maxCount: 2, interval: .seconds(1))
+            .cacheMemoryOnly() // Thumbnail은 메모리만
+            .fade(duration: 0.2)
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+    }
+}
