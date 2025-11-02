6ee4baa624ac6283f55e679efbb2f5d4e5c0ecce
Minbol <bmlee@postech.ac.kr>
2025-11-02 19:56:24 +0900

    feat: CameraStatus namespace 추가

--- HonestHouse/HonestHouse/Sources/Core/Network/DTO/API+Namespace.swift
+++ HonestHouse/HonestHouse/Sources/Core/Network/DTO/API+Namespace.swift
@@ -10,3 +10,4 @@ import Foundation
 enum ImageOperations { }    // 4.7. Image Operations
 enum ShootingControl { }    // 4.8. Shooting Control
 enum ShootingSettings { }   // 4.9. Shooting Settings
+enum CameraStatus { }       // 4.13. Camera Status
