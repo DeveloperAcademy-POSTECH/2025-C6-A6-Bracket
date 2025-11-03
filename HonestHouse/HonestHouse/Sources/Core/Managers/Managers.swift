deb882b89bfa31f82c7464d7c13dfcd64e3a23af
Rama-Moon <moonhygn@gmail.com>
2025-11-03 17:33:44 +0900

    chore: Rebase 이후 코드 수정

--- HonestHouse/HonestHouse/Sources/Core/Managers/Managers.swift
+++ HonestHouse/HonestHouse/Sources/Core/Managers/Managers.swift
@@ -10,19 +10,16 @@ import Foundation
 protocol ManagersType {
     var visionManager: VisionManagerType { get }
     var photoManager: PhotoManagerType { get }
-    var cameraConnectionManager: CameraConnectionManagerType { get }
     var imagePrefetchManager: ImagePrefetchManagerType { get }
 }
 
 final class Managers: ManagersType {
     var visionManager: VisionManagerType
     var photoManager: PhotoManagerType
-    var cameraConnectionManager: CameraConnectionManagerType
     var imagePrefetchManager: ImagePrefetchManagerType
     init() {
         self.visionManager = VisionManager()
         self.photoManager = PhotoManager()
-        self.cameraConnectionManager = CameraConnectionManager()
         self.imagePrefetchManager = ImagePrefetchManager()
     }
 }
@@ -32,6 +29,5 @@ final class Managers: ManagersType {
 final class StubManagers: ManagersType {
     var visionManager: VisionManagerType = StubVisionManager()
     var photoManager: PhotoManagerType = StubPhotoManager()
-    var cameraConnectionManager: CameraConnectionManagerType = StubCameraConnectionManager()
     var imagePrefetchManager: ImagePrefetchManagerType = StubImagePrefetchManager()
 }
