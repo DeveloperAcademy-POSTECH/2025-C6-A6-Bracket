0ebf0a363c08084a7b9ecdca4a420cfb051750ec
dlguszoo <lhjivy1024@gmail.com>
2025-11-03 20:59:46 +0900

    [#55] fix: SelectionGridCellView 수정

--- HonestHouse/HonestHouse/Sources/Presentation/Archive/Component/CachedThumbnailImageView.swift
+++ HonestHouse/HonestHouse/Sources/Presentation/Archive/Component/CachedThumbnailImageView.swift
@@ -11,17 +11,16 @@ import Kingfisher
 /// Thumbnail 이미지만 표시 (그리드, 그룹 대표 이미지 등)
 struct CachedThumbnailImageView: View {
     let url: String
-
+    
     var body: some View {
         KFImage(URL(string: url))
             .placeholder {
-                Color.gray.opacity(0.3)
+                Color.g9
             }
             .retry(maxCount: 2, interval: .seconds(1))
             .cacheMemoryOnly() // Thumbnail은 메모리만
             .fade(duration: 0.2)
             .resizable()
-            .aspectRatio(1, contentMode: .fit)
-            .clipShape(RoundedRectangle(cornerRadius: 8))
+            .aspectRatio(contentMode: .fill)
     }
 }
