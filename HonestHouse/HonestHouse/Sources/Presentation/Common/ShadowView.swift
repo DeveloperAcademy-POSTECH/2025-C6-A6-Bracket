e93e6e6dbba90310242074495128b75f2a825405
dlguszoo <lhjivy1024@gmail.com>
2025-11-04 21:16:59 +0900

    [#55] feat: grid 스켈레톤뷰, Shadow뷰 구현

---
+++ HonestHouse/HonestHouse/Sources/Presentation/Common/ShadowView.swift
@@ -0,0 +1,26 @@
+//
+//  ButtonShadowView.swift
+//  HonestHouse
+//
+//  Created by 이현주 on 11/4/25.
+//
+
+import SwiftUI
+
+struct ShadowView: View {
+    
+    let startBottom: Bool
+    
+    var body: some View {
+        LinearGradient(
+            gradient: Gradient(stops: [
+                .init(color: Color(Color.black).opacity(0), location: 0.0),
+                .init(color: Color(Color.black), location: 1.0)
+            ]),
+            startPoint: startBottom ? .top : .bottom,
+            endPoint: startBottom ? .bottom : .top
+        )
+        .frame(height: 120)
+    }
+}
+
