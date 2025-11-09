43935914806c1d719185ead376a3308c11707316
realhsb <realhsb@gmail.com>
2025-11-09 22:22:34 +0900

    design: Circular Wheel Picker에서 사용되는 그라디언트 정의

---
+++ HonestHouse/HonestHouse/Sources/Extension/Gradient+Extension.swift
@@ -0,0 +1,33 @@
+//
+//  Gradient+Extension.swift
+//  HonestHouse
+//
+//  Created by Subeen on 11/9/25.
+//
+
+import SwiftUI
+
+extension LinearGradient {
+    
+    static let magentaGreenGradient: LinearGradient = .init(
+        colors: [
+            .init(hex: "FF5CC6"),
+            .init(hex: "FFFFFF"),
+            .init(hex: "FFFFFF"),
+            .init(hex: "59FF6D")
+        ],
+        startPoint: .topLeading,
+        endPoint: .bottomTrailing
+    )
+    
+    static let colorTemperatureGradient: LinearGradient = .init(
+        colors: [
+            .init(hex: "36BCFF"),
+            .init(hex: "FFFFFF"),
+            .init(hex: "FFFFFF"),
+            .init(hex: "FFCD38"),
+        ],
+        startPoint: .bottomLeading,
+        endPoint: .topTrailing
+    )
+}
