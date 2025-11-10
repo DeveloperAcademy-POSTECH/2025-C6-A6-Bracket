7b19a5240c58d58ba8594ea4037e6b21a3f7f860
Minbol <bmlee@postech.ac.kr>
2025-11-10 15:11:00 +0900

    fix: togglePreset 삭제

--- HonestHouse/HonestHouse/Sources/Presentation/Trishot/ViewModel/TrishotSettingViewModel.swift
+++ HonestHouse/HonestHouse/Sources/Presentation/Trishot/ViewModel/TrishotSettingViewModel.swift
@@ -12,7 +12,6 @@ import CoreData
 enum TrishotSettingAction {
     case goToTrishotSelection(order: Int)
     case goToTrishotActivation
-    case togglePreset(UUID)
 }
 
 @Observable
@@ -57,44 +56,6 @@ extension TrishotSettingViewModel {
 
         case .goToTrishotActivation:
             container.navigationRouter.push(to: .trishotActivation)
-
-        case .togglePreset(let id):
-            togglePresetSelection(id)
-        }
-    }
-
-    private func togglePresetSelection(_ presetId: UUID) {
-        let isCurrentlyActivated = isPresetActivated(presetId)
-
-        if isCurrentlyActivated && activatedPresets.count <= 2 {
-            if activatedPresets.count == 1 {
-                error = .insufficientPresets
-                return
-            }
-
-            let deactivatedPresets = allSelectedPresets.filter { preset in
-                !activatedPresets.contains { $0.id == preset.id }
-            }
-
-            guard let presetToActivate = deactivatedPresets.first else {
-                error = .noDeactivatedPresetAvailable
-                return
-            }
-
-            do {
-                try container.managers.presetManager.toggleSelectedPresetActivation(presetId: presetId)
-                try container.managers.presetManager.toggleSelectedPresetActivation(presetId: presetToActivate.id)
-                loadPresets()
-            } catch {
-                handleError(error)
-            }
-        } else {
-            do {
-                try container.managers.presetManager.toggleSelectedPresetActivation(presetId: presetId)
-                loadPresets()
-            } catch {
-                handleError(error)
-            }
         }
     }
 }
