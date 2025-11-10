2545ba73e69a4c3767db0e1ad39f17b1021a6350
dlguszoo <lhjivy1024@gmail.com>
2025-11-11 01:36:18 +0900

    [#92] fix: zoomableGesture 수정자 DetailView ProgressiveDisplayImageView에 적용

--- HonestHouse/HonestHouse/Sources/Presentation/Archive/View/PhotoSelectionDetailView.swift
+++ HonestHouse/HonestHouse/Sources/Presentation/Archive/View/PhotoSelectionDetailView.swift
@@ -47,15 +47,11 @@ struct PhotoSelectionDetailView: View {
 
     private func photoDetailView(photo: Photo) -> some View {
         ZStack(alignment: .topLeading) {
-            VStack(spacing: 0) {
-                Spacer()
-                
-                ProgressiveDisplayImageView(
-                    photo: photo
-                )
-                
-                Spacer()
-            }
+            
+            ProgressiveDisplayImageView(
+                photo: photo
+            )
+            .zoomableGesture()
             
             VStack(spacing: 0) {
                 selectionButtonView(photo: photo)
