4304e234feeee68bd889f03f540a4b7472640e70
Rama-Moon <moonhygn@gmail.com>
2025-11-16 23:00:05 +0900

    feat: Setting 뷰 UI 구현, 약관 Constants 추가

---
+++ HonestHouse/HonestHouse/Sources/Presentation/Setting/SettingOption.swift
@@ -0,0 +1,53 @@
+//
+//  SettingList.swift
+//  HonestHouse
+//
+//  Created by Rama on 11/16/25.
+//
+
+import SwiftUI
+
+enum SettingOption: CaseIterable {
+    case connectCamera
+    case termsOfService
+    case privacyPolicy
+    case contact
+    case team
+    case instagram
+    
+    var title: String {
+        switch self {
+        case .connectCamera:
+            return "카메라 연결"
+        case .termsOfService:
+            return "서비스 이용약관"
+        case .privacyPolicy:
+            return "개인정보 처리방침"
+        case .contact:
+            return "문의하기"
+        case .team:
+            return "떳떳 Team"
+        case .instagram:
+            return "떳떳z Instagram"
+        }
+    }
+    
+    @ViewBuilder
+    var destination: some View {
+        switch self {
+        case .instagram, .connectCamera:
+            EmptyView()
+        default:
+            InfoTextView(type: self)
+        }
+    }
+    
+    var externalURL: URL? {
+        switch self {
+        case .instagram:
+            return URL(string: "http://instagram.com/bracket.house")
+        default:
+            return nil
+        }
+    }
+}
