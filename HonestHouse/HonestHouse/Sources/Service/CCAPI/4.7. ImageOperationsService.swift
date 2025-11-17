afac70f1c4c389afd133d4ac3d3169c8f86fd3f4
dlguszoo <lhjivy1024@gmail.com>
2025-11-17 20:23:15 +0900

    [#122] fix: buildContentListURL에서 baseArchiveURL -> baseURL로 수정

--- HonestHouse/HonestHouse/Sources/Service/CCAPI/4.7. ImageOperationsService.swift
+++ HonestHouse/HonestHouse/Sources/Service/CCAPI/4.7. ImageOperationsService.swift
@@ -96,7 +96,7 @@ final class ImageOperationsService: BaseService, ImageOperationsServiceType {
         order: String
     ) throws -> URL {
         let version = CameraType.current?.imageOperationsVersion ?? .ver100
-        let urlString = "\(BaseURLConstants.baseArchiveURL)\(version.description)/contents/\(storage)/\(directory)?type=\(type)&kind=\(kind)&order=\(order)"
+        let urlString = "\(BaseURLConstants.baseURL)\(version.description)/contents/\(storage)/\(directory)?type=\(type)&kind=\(kind)&order=\(order)"
 
         guard let url = URL(string: urlString) else {
             throw CCAPIError.invalidURL
