9a6125d667078a67a30426f9f75ea0e86152c51c
Rama-Moon <moonhygn@gmail.com>
2025-11-18 20:46:34 +0900

    feat: keyboard 숨김 extension 추가

--- HonestHouse/HonestHouse/Sources/Extension/View+Extension.swift
+++ HonestHouse/HonestHouse/Sources/Extension/View+Extension.swift
@@ -147,4 +147,8 @@ extension View {
             self
         }
     }
+    
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
 }
